9b4a216f0c8959fa8e893a307b1715d514d9aaf5
Yamin Ruiz <Yamin_Ruiz@epam.com>
2020-05-25 17:23:02 -0500

    Add variables for ranges lists

--- modules/network/nsg/sr/variables.tf
+++ modules/network/nsg/sr/variables.tf
@@ -1,6 +1,12 @@
 variable "name" {
-  description = "NThe name of the security rule. This needs to be unique across all Rules in the Network Security Group"
+  description = "The name of the security rule. This needs to be unique across all Rules in the Network Security Group"
   type        = string
+}
+
+variable "description" {
+  description = "A description for this rule. Restricted to 140 characters"
+  type        = string
+  default     = ""
 }
 
 variable "rg_name" {
@@ -36,19 +42,47 @@ variable "protocol" {
 variable "source_port_range" {
   description = "Source Port or Range. Integer or range between 0 and 65535 or * to match any. This is required if source_port_ranges is not specified"
   type        = string
+  default     = null
+}
+
+variable "source_port_ranges" {
+  description = "List of source ports or port ranges. This is required if source_port_range is not specified"
+  type        = list(string)
+  default     = null
 }
 
 variable "destination_port_range" {
   description = "Destination Port or Range. Integer or range between 0 and 65535 or * to match any. This is required if destination_port_ranges is not specified"
   type        = string
+  default     = null
+}
+
+variable "destination_port_ranges" {
+  description = "List of destination ports or port ranges. This is required if destination_port_range is not specified"
+  type        = list(string)
+  default     = null
 }
 
 variable "source_address_prefix" {
-  description = "CIDR or source IP range or * to match any IP. Tags such as ‘VirtualNetwork’, ‘AzureLoadBalancer’ and ‘Internet’ can also be used"
+  description = "CIDR or source IP range or * to match any IP. Tags such as ‘VirtualNetwork’, ‘AzureLoadBalancer’ and ‘Internet’ can also be used. This is required if source_address_prefixes is not specified"
   type        = string
+  default     = null
+}
+
+variable "source_address_prefixes" {
+  description = "List of source address prefixes. Tags may not be used. This is required if source_address_prefix is not specified"
+  type        = list(string)
+  default     = null
 }
 
 variable "destination_address_prefix" {
-  description = "CIDR or destination IP range or * to match any IP. Tags such as ‘VirtualNetwork’, ‘AzureLoadBalancer’ and ‘Internet’ can also be used"
+  description = "CIDR or destination IP range or * to match any IP. Tags such as ‘VirtualNetwork’, ‘AzureLoadBalancer’ and ‘Internet’ can also be used. This is required if destination_address_prefixes is not specified"
   type        = string
+  default     = null
+}
+
+variable "destination_address_prefixes" {
+  description = "List of destination address prefixes. Tags may not be used. This is required if destination_address_prefix is not specified"
+  type        = list(string)
+  default     = null
 }
