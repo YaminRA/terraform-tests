491c136f17602a282e2ac079e66c736210ea9391
Yamin Ruiz <Yamin_Ruiz@epam.com>
2020-06-02 00:43:09 -0500

    Make Solr a System Service

--- scripts/provision_solr_vm.sh
+++ scripts/provision_solr_vm.sh
@@ -15,9 +15,33 @@ if [ "$solr_status" != "200" ]
 then
     yum update
     yum install -y java
-    wget https://archive.apache.org/dist/lucene/solr/6.6.2/solr-6.6.2.tgz
+    wget --no-check-certificate https://archive.apache.org/dist/lucene/solr/6.6.2/solr-6.6.2.tgz
     tar xzf solr-6.6.2.tgz solr-6.6.2/bin/install_solr_service.sh --strip-components=2
-    ./install_solr_service.sh solr-6.6.2.tgz -d "$solr_writable_files"
+    ./install_solr_service.sh solr-6.6.2.tgz -d "$solr_writable_files" -n
+    rm -f /etc/init.d/solr
+    touch /etc/systemd/system/solr.service
+    cat > /etc/systemd/system/solr.service <<EOF
+[Unit]
+Description=Apache Solr Server
+
+[Service]
+Type=forking
+User=solr
+Environment=SOLR_INCLUDE=/etc/default/solr.in.sh
+ExecStart=/opt/solr/bin/solr start
+ExecStop=/opt/solr/bin/solr stop
+Restart=on-failure
+LimitNOFILE=65000
+LimitNPROC=65000
+TimeoutSec=180s
+
+[Install]
+WantedBy=multi-user.target
+EOF
+    systemctl daemon-reload
+    systemctl enable solr
+    systemctl start solr
+    systemctl status solr
 else
     echo "Solr Server is already installed or another service is using port 8983"
 fi
